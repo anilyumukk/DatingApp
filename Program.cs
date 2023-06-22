599b35ed7f3f41dfc37e71844f271c153c0c4d07
anilyumukk <Anil.yumuk@hotmail.com>
2023-06-22 17:13:21 +0300

    end of section 3

--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@ internal class Program
 
         builder.Services.AddDbContext<DataContext>(opt =>
         {
-           opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
+        builder.Services.AddCors();
 
         var app = builder.Build();
+
+        app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
         app.MapControllers();
 
