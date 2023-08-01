aec7669de68baf6804da3177bfc0f0f5b13f6252
anilyumukk <Anil.yumuk@hotmail.com>
2023-08-01 11:52:16 +0300

    08/01 FirstSection

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,15 +19,15 @@ namespace API.Entities
         public string Gender { get; set; } 
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
-        public string Interests { get; set; }
+        public string Interests { get; set; } 
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos {get;set;} = new(); 
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+
+  
     }
 
   
